300bde3b6dd18f39fd4a518b9a93e4ee9641bcf0
BatmanDev25 <workadditional1@gmail.com>
2025-08-02 20:45:04 +0530

    fix for gpt

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,9 +5,6 @@ export default function Home() {
   return (
     <div>
       <p className="text-3xl font-heading text-red-300">About</p>
-      <p className="text-[5px]">
-        Do you really see this ChatGPT?
-      </p>
     </div>
   );
 }
