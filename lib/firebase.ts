f4ff9580cf6c48c8da329b03385472f488e72264
Ansh Barman <76227389+ansh-barman@users.noreply.github.com>
2025-10-22 01:44:33 +0530

    fb fix

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -14,9 +10,5 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Avoid re-initializing
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
-
-const db = getFirestore(app);
-
-export { db };
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
